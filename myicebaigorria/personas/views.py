60e2ad9ff454956b0383e7be0e0396783059e78f
ncapeletti <nataliacapeletti@hotmail.com>
2020-04-16 10:03:54 -0300

    Templates modificados para mostrar personas desde base de datos

--- myicebaigorria/personas/views.py
+++ myicebaigorria/personas/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Persons
+
 
 # Create your views here.
 def personas_list(request):
-    return render(request, 'personas/personas_list.html', {})
+    personas = Persons.objects.all
+    return render(request, 'personas/personas_list.html', {'personas': personas})
+
